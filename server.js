b86ab2da3133e5d37d37b3d1bfa9d5bdd025a78c
buzapeter12345 <buzapeter12345@gmail.com>
2023-11-09 11:09:21 +0100

    update 1.1

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const io = new Server(server);
 
 io.on("connection", (s) => {
   s.on("msg", (msg, felhasznalo) => {
+    if (felhasznalo == "") {
+      var felhasznalo = "Vendég";
+    }
     var uzenet = felhasznalo + ": " + msg;
     io.emit("msg", uzenet);
   });
@@ -17,6 +20,6 @@ io.on("connection", (s) => {
   });
 });
 
-server.listen(80, () => {
-  console.log("server running at http://localhost:80");
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
 });
