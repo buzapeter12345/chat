15cb63f19f508eacfd85661db390903c211d4bfb
buzapeter12345 <buzapeter12345@gmail.com>
2023-11-09 19:52:17 +0100

    a

--- client/client.js
+++ client/client.js
@@ -1,44 +1,38 @@
-const socket = io();
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
-const remc = document.getElementById("e");
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const messages = document.getElementById("messages");
-const username = document.getElementById("username");
+const app = express();
+app.use(express.static("client"));
+const server = createServer(app);
+const io = new Server(server);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit("msg", input.value, username.value);
-    input.value = "";
-    if (username.value) {
-      username.style.display = "none";
+// Create an array to store chat messages
+const chatMessages = [];
+
+io.on("connection", (socket) => {
+  // Send previous chat messages to the connected client
+  socket.emit("chatHistory", chatMessages);
+
+  socket.on("msg", (msg, felhasznalo) => {
+    if (felhasznalo == "") {
+      felhasznalo = "Vendég";
     }
-  }
+    const uzenet = `${felhasznalo}: ${msg}`;
+    const uzenetHTML = `<h1>${felhasznalo}: </h1> <h3>${msg}</h3>`;
+    
+    // Store the message in the chatMessages array
+    chatMessages.push(uzenetHTML);
+
+    // Broadcast the message to all connected clients
+    io.emit("msg", uzenetHTML);
+  });
+
+  socket.on("cur", (msg) => {
+    io.emit("cur", msg);
+  });
 });
 
-function f(e) {
-  socket.emit("cur", { x: e.x, y: e.y });
-  return false;
-}
-
-socket.on("msg", (msg) => {
-  const item = document.createElement("li");
-  if (msg.includes("http://") || msg.includes("https://")) {
-    const link = document.createElement("a");
-    link.setAttribute("href", msg);
-    link.setAttribute("target", "_blank");
-    link.textContent = "LINK";
-    item.appendChild(link);
-  } else {
-    item.innerHTML = msg;
-  }
-  messages.appendChild(item);
-
-  window.scrollTo(0, document.body.scrollHeight);
-});
-
-socket.on("cur", (msg) => {
-  remc.style.left = `${msg.x - 6}px`;
-  remc.style.top = `${msg.y - 6}px`;
+server.listen(80, () => {
+  console.log("Server is running at http://localhost:80");
 });
--- server.js
+++ server.js
@@ -21,6 +21,6 @@ io.on("connection", (s) => {
   });
 });
 
-server.listen(80, () => {
-  console.log("server  running at http://localhost:80");
+server.listen(3000, () => {
+  console.log("server  running at http://localhost:3000");
 });
